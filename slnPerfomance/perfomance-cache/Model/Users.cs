09338c6af039a78856f5df5ac81d4faf64d8ec30
Leonardo de Souza Mazzuca <124372925+Leonardo-Mazzuca@users.noreply.github.com>
2025-09-10 20:07:12 -0300

    feat: adicionando arquivos

---
+++ slnPerfomance/perfomance-cache/Model/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace perfomance_cache.Model
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public DateTime UltimoAcesso { get; set; }
+    }
+}
